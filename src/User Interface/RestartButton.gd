59c8433f617ccd238499c0841a25e1235686384e
JAAG4 <45705167+JAAG4@users.noreply.github.com>
2019-10-11 12:13:19 -0400

    Organized the Project folder

--- src/User Interface/RestartButton.gd
+++ src/User Interface/RestartButton.gd
@@ -3,5 +3,6 @@ extends Button
 
 
 func _on_Button_button_up():
-	get_tree().change_scene("res://src/Restart.tscn")
+#	get_tree().change_scene("res://src/Restart.tscn")
+	get_tree().reload_current_scene()
 	#This re-start button feature was added AFTER Ludum Dare
